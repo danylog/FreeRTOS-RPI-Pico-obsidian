e21f7aac5424e47a80b0924377f49e96ac85c92f
Danylo Galytskyy <work.danylo.galytskyy@gmail.com>
2025-04-05 22:04:49 +0200

    add submodule

---
+++ my-project/build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/Users/danylo/Desktop/my-project/build/_deps/picotool/"
+};
---
+++ my-project/build/_deps/picotool-build/lib/mbedtls/library/CMakeFiles/mbedx509.dir/pkcs11.c.o.d
@@ -0,0 +1,15 @@
+lib/mbedtls/library/CMakeFiles/mbedx509.dir/pkcs11.c.o: \
+  /Users/danylo/pico/pico-sdk/lib/mbedtls/library/pkcs11.c \
+  /Users/danylo/pico/pico-sdk/lib/mbedtls/include/mbedtls/pkcs11.h \
+  /Users/danylo/pico/pico-sdk/lib/mbedtls/include/mbedtls/config.h \
+  /Users/danylo/pico/pico-sdk/lib/mbedtls/include/mbedtls/check_config.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/cdefs.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/_symbol_aliasing.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/_posix_availability.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/17/include/ptrcheck.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/machine/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/i386/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/i386/_limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sys/syslimits.h
